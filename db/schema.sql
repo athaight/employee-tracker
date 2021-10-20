5edeb0326ef170fe6b20ef9a5830025c811be187
Andrew Haight <haight.haight@gmail.com>
2021-10-20 15:31:36 -0700

    schema built

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+USE employees;
+CREATE TABLE department (
+  department_name VARCHAR(30) NOT NULL,
+  department_id INTEGER PRIMARY KEY AUTO_INCREMENT
+);
+CREATE TABLE role (
+  role_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL
+);
+CREATE TABLE employee (
+  employee_id INTEGER NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  manager_id INTEGER NOT NULL
+);

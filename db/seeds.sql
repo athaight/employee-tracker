fae3de0a1a499f9becd0d148f3a3f6f0e51ccfa3
Andrew Haight <haight.haight@gmail.com>
2021-10-20 16:06:16 -0700

    seeds build correct

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,28 +1,25 @@
-use employees;
-
-INSERT INTO department(department_name, department_id)
-VALUES
-("Sales", 1),
-("Engineering", 2),
-("Finance", 3),
-("Legal", 4);
+INSERT INTO department (department_name, department_id)
+VALUES ('Sales', 1),
+       ('Engineering', 2),
+       ('Finance,', 3),
+       ('Legal', 4);
 
 INSERT INTO role (title, salary, department_id)
-VALUES
-("Manager", 90000, 1),
-("Sales", 120000, 1),
-("Senior Engineer", 150000, 2),
-("Engineer", 120000, 2),
-("Intern", 20000, 3),
-("Finance", 125000, 3)
+VALUES ('Sales Lead', 100000, 1),
+       ('Salesperson', 80000, 1),
+       ('Lead Engineer', 150000, 2),
+       ('Software Engineer', 120000, 2),
+       ('Account Manager', 160000, 3),
+       ('Accountant', 125000, 3),
+       ('Legal Team Lead', 250000, 4),
+       ('Lawyer', 190000, 4);
 
 INSERT INTO employee (employee_id, first_name, last_name, role_id, manager_id)
-VALUES
-(1, "Kevin", "Pug", 1, 1),
-(2, "Birdie" ,"Cat", 2, 1),
-(3, "Penny", "Girl", 4, 3),
-(4, "Alvin", "Chipmunk", 3, 1),
-(5, "William", "Brown", 5, 1),
-(6, "James", "Wallace", 6, 5),
-(7, "Bruce", "Campbell", 7, 1),
-(8, "James", "Spader", 8, 7);
+VALUES (1, 'John', 'Doe', 1, 1),
+       (2, 'Mike', 'Chan', 2, 1),
+       (3, 'Ashley', 'Rodriguez', 3, 3),
+       (4, 'Kevin', 'Tupik', 4, 3),
+       (5, 'Kunal', 'Singh', 5, 1),
+       (6, 'Malia', 'Brown', 6, 5),
+       (7, 'Sarah', 'Lourd', 7, 3),
+       (8, 'Tom', 'Allen', 8, 7);
